491cda7cfb8ea88bcfe2aef27d5cc90209c8b489
JesusCoronel99 <53198603+JesusCoronel99@users.noreply.github.com>
2019-07-25 09:27:07 -0500

    Add files via upload

---
+++ IMCyCuadratica.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Ingrese el valor de a ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de b ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de c ");
+            int c = int.Parse(Console.ReadLine());
+
+            double discriminante = (b * b) - (4 * a * c);
+            double x1 = (-b) + Math.Sqrt(discriminante) / (2 * a);
+            double x2 = (-b) - Math.Sqrt(discriminante) / (2 * a);
+
+            if (discriminante == 0)
+            {
+                Console.WriteLine("La solucion existe y es unica");
+            }
+            else if (discriminante > 0)
+            {
+                Console.WriteLine("Existen dos posibles soluciones correctas");
+                Console.WriteLine("El valor de x1 es " + x1);
+                Console.WriteLine("El valor de x2 es " + x2);
+            }
+            else
+            {
+                Console.WriteLine("No es posible calcular la solucion");
+                
+           }
+           */
+           /*
+            Console.WriteLine("Ingrese su peso");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su estatura");
+            double estatura = double.Parse(Console.ReadLine());
+
+            double imc = peso / Math.Pow(estatura, 2);
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Peso inferior a lo normal");
+            }
+            else if (18.5 <= imc && imc <= 24.9)
+            {
+                Console.WriteLine("Peso Normal");
+            }
+            else if (25 <= imc && imc <= 29.9)
+            {
+                Console.WriteLine("Peso superior a lo Normal");
+            }
+            else if (imc > 30)
+            {
+                Console.WriteLine("Obesidad");
+            }
+            */
+        }
+    }
+}
