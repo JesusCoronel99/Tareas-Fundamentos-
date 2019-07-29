7794112396dc7b7d40de3b797ac2fe74fa49780b
JesusCoronel99 <53198603+JesusCoronel99@users.noreply.github.com>
2019-07-28 23:45:30 -0500

    Add files via upload

---
+++ SMMLV.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double salariominimo = 925148;
+            Console.Write("Ingrese el salario que recibe ");
+            double smmlv = double.Parse(Console.ReadLine());
+
+            if (smmlv < salariominimo * 2)
+            {
+                Console.WriteLine("Su tarifa correspondiente es la A");
+            }
+            else if (salariominimo * 2 <= smmlv && smmlv < salariominimo * 4)
+            {
+                Console.WriteLine("Su tarifa correspondiente es la B");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa correspondiente es la C");
+            }
+        }
+    }
+}
---
+++ SMMLV_CM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salariominimo = 925148;
+            Console.Write("Ingrese el salario que recibe: ");
+            double smmlv = double.Parse(Console.ReadLine());
+
+            if (smmlv < salariominimo * 2)
+            {
+                Console.WriteLine("Usted esta en tarifa A, Su cuota moderadora es de $3.200");
+            }
+            else if (salariominimo * 2 <= smmlv && smmlv < salariominimo * 5)
+            {
+                Console.WriteLine("Usted esta en tarifa B, Su cuota moderadora es de $12.700");
+            }
+            else
+            {
+                Console.WriteLine("Usted esta en tarifa C, Su cuota moderadora es de $35.500");
+            }
+        }
+    }
+}
