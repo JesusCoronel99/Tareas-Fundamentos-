914edf4c585a8c9cab854fb533bc4648a7b7ed23
JesusCoronel99 <53198603+JesusCoronel99@users.noreply.github.com>
2019-08-19 17:39:41 -0500

    Add files via upload

---
+++ TareaDados01.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DadosCasa01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int vidas = 3,turnos = 0, puntos = 0;
+            Console.WriteLine("Vidas Iniciales: " + vidas);
+            while (true)
+            {
+                turnos++;
+                int dado = aleatorio.Next(1, 7);
+                Console.WriteLine("Dado: " + dado);
+                puntos += dado;
+                Console.WriteLine("Puntos: " + puntos);
+                if (turnos %2 == 0)
+                {
+                    Console.WriteLine("Has Perdido Una Vida");
+                    vidas -= 1;
+                    Console.WriteLine("Vidas: " + vidas);
+                }
+                if (turnos %3 == 0)
+                {
+                    int dado1 = aleatorio.Next(1, 7);
+                    Console.WriteLine("Nuevo Dado: " + dado1);
+                    Console.WriteLine("Nuevo Dado: " + dado);         
+                    if (dado1 == dado)
+                    {
+                        Console.WriteLine("Has Ganado Una Vida");
+                        vidas += 1;
+                        Console.WriteLine("Vidas: " + vidas);
+                    }
+                }
+                if (vidas == 0)
+                {
+                    Console.WriteLine("GAME OVER");
+                    break;
+                }
+                Console.WriteLine("Desea Continuar (s/n) ");
+                if (Console.ReadLine() != "s") break;
+            }
+            Console.WriteLine("Puntos: " + puntos);
+            Console.WriteLine("Gracias Por Jugar");
+
+        }
+    }
+}
