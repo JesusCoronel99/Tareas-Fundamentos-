341f3c7385de11af2bce988a9c4343ed331c44c2
JesusCoronel99 <53198603+JesusCoronel99@users.noreply.github.com>
2019-08-19 13:00:55 -0500

    Add files via upload

---
+++ Dado10a12.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TareaDado
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random aleatorio = new Random();
+            int total = 0, contador = 0, condicion = 0; 
+            while (true)
+            {
+                contador++;
+                int dado = aleatorio.Next(1, 13);
+                Console.WriteLine("Dado: " + dado);
+                if (dado == 10) condicion += 1;
+                if (contador <= 3)
+                {
+                    if (dado ==12 && condicion==1)
+                    {
+                        Console.WriteLine("Ha Ganado");
+                        Console.WriteLine("Total: " + total);
+                        break;
+                    }
+                    total += dado;
+                    Console.WriteLine("Total: " + total);
+                    Console.WriteLine("Desea Continuar (s/n)");
+                    if (Console.ReadLine() != "s") break;
+                }
+                else if (dado %2 == 0)
+                {
+                    total += dado;
+                    Console.WriteLine("Total: " + total);
+                    if (dado == 12 && condicion == 1)
+                    {
+                        Console.WriteLine("Ha Ganado");
+                        Console.WriteLine("Total: " + total);
+                        break;
+                    }
+                    if (total >= 100) Console.WriteLine("Ha Ganado");
+                    Console.WriteLine("Desea Continuar (s/n)");
+                    if (Console.ReadLine() != "s") break;
+                }
+                else
+                {
+                    total += dado;
+                    Console.WriteLine("Total: " + total);
+                    Console.WriteLine("Ha Sacado Numero Impar, Queda ELIMINADO");
+                    break;
+                }
+            }
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("Fin del Juego");
+        }
+    }
+}
