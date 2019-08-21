93cbce35a55365ebc30bd87ef7541e7e7c222756
JesusCoronel99 <53198603+JesusCoronel99@users.noreply.github.com>
2019-08-21 10:44:51 -0500

    Add files via upload

---
+++ Captcha.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EjercicioClase20agosto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random aleatorio = new Random();
+            int a = aleatorio.Next(0, 10);
+            int b = aleatorio.Next(0, 10);
+            int contador = 1;
+            Console.WriteLine("A: " + a + " B: " + b);
+            int sumacorrecta = a + b;
+            Console.Write("Ingrese la suma entre a y b: ");
+            int suma = int.Parse(Console.ReadLine());
+
+            while (sumacorrecta != suma)
+            {
+                a = aleatorio.Next(0, 10);
+                b = aleatorio.Next(0, 10);
+                Console.WriteLine("A: " + a + " B: " + b);
+                Console.WriteLine("Incorrecto, trate de nuevo");
+                Console.WriteLine("Ingrese la suma entre a y b: ");
+                sumacorrecta = a + b;
+                suma = int.Parse(Console.ReadLine());
+                contador++;
+                if (contador == 3 && sumacorrecta != suma)
+                {
+                    Console.WriteLine("Demasiados Intentos");
+                    break;
+                }
+                else if (contador <3 && sumacorrecta == suma)
+                {
+                    Console.WriteLine("Acceso Permitido");
+                    break;
+                }
+            }
+        }
+    }
+}
